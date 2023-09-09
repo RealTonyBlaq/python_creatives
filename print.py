63a00b918b13f8af8cda22a9b07292968739698f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-09 23:17:12 +0100

    print lists

--- print.py
+++ print.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 a = 0
-while a < 10:
+while a < 6:
 	print(a, end=' ')
 	a = a + 1
 print()
 lists = [1, 2, 3, 4, 5]
 for i in range(len(lists)):
+	print(lists[i], end='\t  ')
 	print(lists[i])
 print(0, end=' ')
 for i in range(len(lists)):
