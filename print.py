b5cad020da26d17fbd562ff1ca8431ddfbf19742
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-09 17:46:15 +0100

    testing lists in python

---
+++ print.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+a = 0
+while a < 10:
+	print(a, end=' ')
+	a = a + 1
+print()
+lists = [1, 2, 3, 4, 5]
+for i in range(len(lists)):
+	print(lists[i])
+print(0, end=' ')
+for i in range(len(lists)):
+	print(lists[i], end=' ')
+print()
